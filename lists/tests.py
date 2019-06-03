a03700ae1f0c04b418d820c62f3c69a221174cec
Paul van Santen <paul@solarmonkey.nl>
2019-06-03 14:58:22 +0200

    First item in the todo table

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,9 @@ from django.test import TestCase
 from lists.views import home_page
 
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
@@ -13,3 +16,8 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html') 
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
