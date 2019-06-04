1e51306e3ee8479e2f1703e600ad30559455e2df
Paul van Santen <paul@solarmonkey.nl>
2019-06-04 11:07:02 +0200

    A new URL, view and template for lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,12 +6,12 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):  
+class NewVisitorTest(LiveServerTestCase):
 
-    def setUp(self):  
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
@@ -27,7 +27,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):  
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
