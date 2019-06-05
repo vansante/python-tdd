c4b46eab5ee08ac645cdadf25e1b7bb164dabf04
Paul van Santen <paul@solarmonkey.nl>
2019-06-05 09:52:21 +0200

    rename all item input ids and names. still broken

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -16,6 +16,9 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
+
     def wait_for(self, fn):
         start_time = time.time()
         while True:
