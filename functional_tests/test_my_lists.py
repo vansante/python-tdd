7d25d360d523fe57fd6acb76e7daf953de6413b0
Paul van Santen <pvansanten@gmail.com>
2019-06-11 11:21:35 +0200

    Add wait for decorator for functional tests

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -21,7 +21,7 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
-     def test_logged_in_users_lists_are_saved_as_my_lists(self):
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'edith@example.com'
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_out(email)
