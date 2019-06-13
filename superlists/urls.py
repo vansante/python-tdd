c2ae65c69221a04cf2593543edf34820c20c8182
Paul van Santen <pvansanten@gmail.com>
2019-06-13 11:49:19 +0200

    Add a basic REST API

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
 from accounts import urls as account_urls
+from lists import api_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
     url(r'^accounts/', include(account_urls)),
+    url(r'^api/', include(api_urls)),
 ]
